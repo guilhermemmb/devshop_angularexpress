984f1fb763da2eac6ca166b5fd261e37bc3b0ac8
Guilherme Bomfim <guilhermemmb@gmail.com>
2015-05-08 01:24:43 -0300

    subindo o primeiro test

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,15 +1,26 @@
 var userService = require('../services/userService');
 
 function getAll(req, res) {
-	var page = req.query.page || 1;
-	var size = req.query.size || 30;
+	var page = req.query.page;
+	var size = req.query.size;
+
+	if(!page || page < 1) {
+		res.status(400).send("invalid page");
+		return;
+	}
+
+	if(!size || size < 1) {
+		res.status(400).send("invalid page size");
+		return;
+	}
+
+	var size = req.query.size;
 
 	userService.getOrgUsers(page, size).then(function (data) {
 		res.json(data);
 	}, function (err) {
 		res.status(500).send(JSON.stringify(err));
 	});
-
 };
 
 module.exports = {
--- tests/api/controllers/users.spec.js
+++ tests/api/controllers/users.spec.js
@@ -1,37 +1,106 @@
-var expect = require('expect.js'),
+var expect = require('chai').expect,
+	sinon = require('sinon'),
+	q  = require('q'),
 	proxyquire = require('proxyquire');
 
-//function waitForCompletion(done, callback) {
-//	setTimeout(function() {
-//		callback();
-//		done();
-//	}, 10);
-//}
-
-describe('user controller', function() {
+describe('user controller', function () {
 	var sut;
 	var reqMock;
 	var resMock;
+	var userServiceMock;
 
 	beforeEach(function () {
-		reqMock = {},
-			resMock = {},
-			usersMock = {};
+		reqMock = {}, resMock = {}, userServiceMock = {};
 
-		console.log();
-		sut = proxyquire('../../../api/controllers', {
-			'./users': usersMock
+		sut = proxyquire('../../../api/controllers/users', {
+			'../services/userService': userServiceMock
 		});
 	});
 
 	describe('getAll', function () {
-		it('Quando se passa um tamanho de página inválida', function () {
-			// Act
-			sut.loginOnSpotify(reqMock, resMock);
+		describe('erros de parametros', function () {
+			describe('Quando se passa uma página inválida', function () {
+				it('informa que a pagina pedida é invalida', function () {
+					reqMock.query = {
+						page: -1,
+						size: 10
+					};
 
-			// Assert
-			expect(resMock.redirect.called).to.be(true);
-			expect(spotifyMock.getLoginUrl.called).to.be(true);
+					var messageSpy = sinon.spy();
+					resMock.status = sinon.stub().withArgs(400).returns({send: messageSpy});
+
+					sut.getAll(reqMock, resMock);
+
+					expect(messageSpy.firstCall.args[0]).to.equal('invalid page');
+				});
+			});
+
+			describe('Quando se passa um tamanho de página inválida', function () {
+				it('informa que o tamanho é invalido.', function () {
+					reqMock.query = {
+						page: 1,
+						size: -1
+					};
+
+					var messageSpy = sinon.spy();
+					resMock.status = sinon.stub().withArgs(400).returns({send: messageSpy});
+
+					sut.getAll(reqMock, resMock);
+
+					expect(messageSpy.firstCall.args[0]).to.equal('invalid page size');
+				});
+			});
+		});
+		describe('ao chamar o servico', function () {
+			var dfd = q.defer();
+			beforeEach(function (){
+			});
+			describe('com os parametros esperados', function () {
+				it(' retorna o obj esperado', function () {
+					userServiceMock.getOrgUsers = sinon.stub().withArgs(1, 10).returns(dfd.promise);
+
+					reqMock.query = {
+						page: 1,
+						size: 10
+					};
+					var expectedData = {
+						element: 1
+					};
+
+					resMock.json = sinon.spy();
+
+					sut.getAll(reqMock, resMock);
+
+					dfd.resolve({element: 1});
+
+					setTimeout(function () {
+						expect(resMock.json.firstCall.args[0]).to.equal(expectedData);
+					},0);
+				});
+			});
+			describe('e o servico falha', function () {
+				it(' retorna erro 500 com a mensagem de erro', function () {
+					userServiceMock.getOrgUsers = sinon.stub().returns(dfd.promise);
+
+					reqMock.query = {
+						page: 10,
+						size: 10
+					};
+
+					var messageSpy = sinon.spy();
+					var EXPECTED_MESSAGE = 'hue!';
+
+					resMock.status = sinon.stub().withArgs(500).returns({send: messageSpy});
+
+					sut.getAll(reqMock, resMock);
+
+					dfd.reject(EXPECTED_MESSAGE);
+
+					setTimeout(function () {
+						expect(messageSpy.firstCall.args[0]).to.equal(EXPECTED_MESSAGE);
+					},0);
+				});
+			});
 		});
 	});
 });
